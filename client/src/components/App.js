726c87746a0a287f2521bb1ecb9cabe05f957aa7
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-05 21:43:20 -0400

    Add Nav component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,14 @@
 import React from "react";
 import Search from "./pages/Search";
 import NoMatch from "./pages/404";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import TypoGraphy from "@material-ui/core/Typography";
-// import NoMatch from "./pages/NoMatch";
-// import Nav from "./components/Nav";
+import Nav from "./layouts/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div>
-        <AppBar color="primary" position="static">
-          <Toolbar>
-            <TypoGraphy variant="title" color="inherit">
-              Boogle Books
-            </TypoGraphy>
-          </Toolbar>
-        </AppBar>
+        <Nav />
         <Switch>
           <Route exact path="/" component={Search} />
           <Route component={NoMatch} />
