aeb6638e33a34de3bfbdf7ece4e7c864b221da7f
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-05 21:25:03 -0400

    Import Material UI, add basic header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Search from "./pages/Search";
 import NoMatch from "./pages/404";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import TypoGraphy from "@material-ui/core/Typography";
 // import NoMatch from "./pages/NoMatch";
 // import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -9,6 +12,13 @@ function App() {
   return (
     <Router>
       <div>
+        <AppBar color="primary" position="static">
+          <Toolbar>
+            <TypoGraphy variant="title" color="inherit">
+              Boogle Books
+            </TypoGraphy>
+          </Toolbar>
+        </AppBar>
         <Switch>
           <Route exact path="/" component={Search} />
           <Route component={NoMatch} />
